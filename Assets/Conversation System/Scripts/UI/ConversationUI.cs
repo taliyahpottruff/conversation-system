7d9e2057bf00e564f80e01563a5b7c3caf63f3f4
Trenton Pottruff <tpottruff@gmail.com>
2021-03-12 05:21:13 -0500

    Basic Trigger

---
+++ Assets/Conversation System/Scripts/UI/ConversationUI.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ConversationSystem.UI {
+    public class ConversationUI : MonoBehaviour {
+        [SerializeField]
+        private TextMeshProUGUI nametag, text;
+
+        private void Start() {
+            Debug.Log("Conversation started!");
+        }
+    }
+}
---
+++ Assets/Conversation System/Triggers/StartTrigger.cs
@@ -0,0 +1,22 @@
+﻿using ConversationSystem.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConversationSystem.Triggers {
+    public class StartTrigger : MonoBehaviour {
+        public float delay;
+
+        private ConversationUI conversationBox;
+
+        private void Start() {
+            StartCoroutine(StartConversation_Coroutine());
+        }
+
+        private IEnumerator StartConversation_Coroutine() {
+            yield return new WaitForSeconds(delay);
+            Debug.Log("Start Conversation...");
+            Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/Conversation"));
+        }
+    }
+}
