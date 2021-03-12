97b1725f9e245faff05b750efabee164b3639d9f
Trenton Pottruff <tpottruff@gmail.com>
2021-03-12 06:40:40 -0500

    Asset Targeting

--- Assets/Conversation System/Editor/ConversationEditorWindow.cs
+++ Assets/Conversation System/Editor/ConversationEditorWindow.cs
@@ -5,6 +5,8 @@ using UnityEditor;
 
 namespace ConversationSystem.Editor {
     public class ConversationEditorWindow : EditorWindow {
+        Conversation target;
+
         Rect window1;
         Rect window2;
 
@@ -16,28 +18,45 @@ namespace ConversationSystem.Editor {
         }
 
         public void Init() {
+            titleContent = new GUIContent("Conversation Editor");
+
             window1 = new Rect(10, 10, 100, 100);
             window2 = new Rect(210, 210, 100, 100);
         }
 
-        private void Awake() {
-            titleContent = new GUIContent("Conversation Editor");
+        private void OnSelectionChange() {
+            var selected = Selection.activeObject;
+            if (selected.GetType() != typeof(DefaultAsset)) {
+                Debug.Log(selected.GetType());
+                target = selected as Conversation;
+            }
         }
 
-        void OnGUI() {
-            DrawNodeCurve(window1, window2); // Here the curve is drawn under the windows
+        private void OnGUI() {
+            if (target != null) {
+                // Node Area
+                GUILayout.BeginArea(new Rect(0, 0, position.width - 250, position.height));
+                DrawNodeCurve(window1, window2); // Here the curve is drawn under the windows
 
-            BeginWindows();
-            window1 = GUI.Window(1, window1, DrawNodeWindow, "Window 1");   // Updates the Rect's when these are dragged
-            window2 = GUI.Window(2, window2, DrawNodeWindow, "Window 2");
-            EndWindows();
+                BeginWindows();
+                window1 = GUI.Window(1, window1, DrawNodeWindow, "Window 1");   // Updates the Rect's when these are dragged
+                window2 = GUI.Window(2, window2, DrawNodeWindow, "Window 2");
+                EndWindows();
+                GUILayout.EndArea();
+
+                // Sidebar Area
+                GUILayout.BeginArea(new Rect(position.width - 250, 0, 250, position.height));
+                GUILayout.Label(target.name);
+                GUILayout.Button("Do Nothing");
+                GUILayout.EndArea();
+            }
         }
 
-        void DrawNodeWindow(int id) {
+        private void DrawNodeWindow(int id) {
             GUI.DragWindow();
         }
 
-        void DrawNodeCurve(Rect start, Rect end) {
+        private void DrawNodeCurve(Rect start, Rect end) {
             Vector3 startPos = new Vector3(start.x + start.width, start.y + start.height / 2, 0);
             Vector3 endPos = new Vector3(end.x, end.y + end.height / 2, 0);
             Vector3 startTan = startPos + Vector3.right * 50;
