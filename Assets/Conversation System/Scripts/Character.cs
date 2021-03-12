117f9e507c74166be4821ea0e036db955f38a2ba
Trenton Pottruff <tpottruff@gmail.com>
2021-03-12 05:31:36 -0500

    Created testing assets

---
+++ Assets/Conversation System/Scripts/Character.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConversationSystem {
+    [CreateAssetMenu(fileName = "New Character", menuName = "Conversation System/Character")]
+    public class Character : ScriptableObject {
+        public new string name = "Name";
+    }
+}
